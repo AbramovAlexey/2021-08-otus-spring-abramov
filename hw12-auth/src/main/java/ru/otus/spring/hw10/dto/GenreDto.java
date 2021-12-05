10ff07aec6d8be02d696b2cbe297dc460d851454
Abramov Alexey <abramov.saracin@yandex.ru>
2021-12-05 21:57:04 +0300

    h12 init

---
+++ hw12-auth/src/main/java/ru/otus/spring/hw10/dto/GenreDto.java
@@ -0,0 +1,13 @@
+package ru.otus.spring.hw10.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GenreDto {
+
+    private String id;
+    private String name;
+
+}
---
+++ hw12-auth/src/main/java/ru/otus/spring/hw10/service/GenreService.java
@@ -0,0 +1,15 @@
+package ru.otus.spring.hw10.service;
+
+import ru.otus.spring.hw10.model.Genre;
+
+import java.util.List;
+
+public interface GenreService {
+
+    String create(String name);
+    void update(String oldName, String newName);
+    Genre readByName(String name);
+    List<Genre> readAll();
+    void deleteByName(String name);
+    public Genre readOrCreate(String name);
+}
