a59e4254e641e0ff1cd721491fb2dfc454446b77
Abramov Alexey <abramov.saracin@yandex.ru>
2021-12-12 18:50:29 +0300

    h12 done

--- hw12-auth/src/main/java/ru/otus/spring/hw12/security/jwt/AuthTokenFilter.java
+++ hw12-auth/src/main/java/ru/otus/spring/hw12/security/jwt/AuthTokenFilter.java
@@ -6,11 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-import ru.otus.spring.hw12.service.UserDetailsServiceImpl;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -28,10 +28,10 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
     private static final String AUTH_HEADER = "Authorization";
     private static final String BEARER_PREFIX = "Bearer";
-    private static final Pattern TOKEN_PATTERN = Pattern.compile(BEARER_PREFIX + "(.+)$");
+    private static final Pattern TOKEN_PATTERN = Pattern.compile(BEARER_PREFIX + " (.+)$");
 
     private final JwtUtils jwtUtils;
-    private final UserDetailsServiceImpl userDetailsService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
---
+++ hw12-auth/src/test/java/ru/otus/spring/hw12/security/BookControllerSecurityTest.java
@@ -0,0 +1,67 @@
+package ru.otus.spring.hw12.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.otus.spring.hw12.rest.BookController;
+import ru.otus.spring.hw12.security.config.WebSecurityConfig;
+import ru.otus.spring.hw12.security.jwt.AuthEntryPointJwt;
+import ru.otus.spring.hw12.security.jwt.AuthTokenFilter;
+import ru.otus.spring.hw12.security.jwt.JwtConfig;
+import ru.otus.spring.hw12.security.jwt.JwtUtils;
+import ru.otus.spring.hw12.service.BookService;
+import ru.otus.spring.hw12.service.DtoConverter;
+
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookController.class)
+@Import({AuthEntryPointJwt.class, JwtConfig.class, AuthTokenFilter.class, BookController.class})
+@ContextConfiguration(classes = {WebSecurityConfig.class})
+public class BookControllerSecurityTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private DtoConverter dtoConverter;
+
+    @MockBean
+    private BookService bookService;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
+
+    @MockBean
+    private JwtUtils jwtUtils;
+
+    @Test
+    void shouldRequireAuthWhenGetList() throws Exception {
+        when(bookService.readAll()).thenReturn(Collections.emptyList());
+        mvc.perform(get("/api/books"))
+           .andExpect(status().is(HttpStatus.UNAUTHORIZED.value()));
+    }
+
+    @Test
+    void shouldAllOkWhenGetListWithToken() throws Exception {
+        String token = "someToken";
+        String user = "admin";
+        when(bookService.readAll()).thenReturn(Collections.emptyList());
+        when(jwtUtils.validateJwtToken(token)).thenReturn(true);
+        when(jwtUtils.getUserNameFromJwtToken(token)).thenReturn(user);
+        when(userDetailsService.loadUserByUsername(user)).thenReturn(new User(user, user, Collections.emptyList()));
+        mvc.perform(get("/api/books").header("Authorization", "Bearer " + token))
+           .andExpect(status().isOk());
+    }
+
+}
